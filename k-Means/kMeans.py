967938d630164625121ef4ad1acf84f5a3448a41
StienoX <stein.bout@xs4all.nl>
2020-02-14 10:57:51 +0100

    Added more oneliners and implemented optimal k function and init function

--- k-Means/kMeans.py
+++ k-Means/kMeans.py
@@ -1,6 +1,8 @@
 from scipy.spatial import distance
-from operator import add
+import numpy as np
+from operator import add, concat
 import functools
+import math
 from import_data import *
 import random
 
@@ -13,14 +15,17 @@ class Cluster:
         self.old_points = None
         self.changed = True
 
-    def move_centroid(self,points):
-        self.centroid = list(map((lambda p: p / len(self.points)),list(map((lambda i: sum(list(map((lambda x: x[i]),points)))),list(range(0,len(points[0])))))))
+    def calc_pos_centroid(self,points):
+        #moves the centroid to the average of the points provided as parameter.
+        self.centroid = list(map((lambda p: p / len(points)),list(map((lambda i: sum(list(map((lambda x: x[i]),points)))),list(range(0,len(points[0])))))))
 
     def recalculate_centroid(self):
-        self.move_centroid(self.points) if len(self.points) else (self.move_centroid(self.old_points) if len(self.old_points) else print("Error: old did not contain any points"))
+        #checks if the centroid has points, if no points are currently available use the old points.
+        self.calc_pos_centroid(self.points) if len(self.points) else print("Error: old did not contain any points")
 
     def check_points_change(self):
-        self.changed = self.points != self.old_points
+        #checks if the points have moved.
+        self.changed = self.points != self.old_points if self.old_points != [] else True
 
     def clear_points(self):
         self.check_points_change()
@@ -28,10 +33,8 @@ class Cluster:
         self.points = []
 
     def calculate_avg_dist_to_centroid(self):
-        dist_sum = 0
-        for point in self.old_points:
-            dist_sum += distance.euclidean(point, self.centroid)
-        return dist_sum / len(self.old_points)
+        # calculates the avg distance to the centroid of the containing points, if a centroid does not have any point it returns infinite!
+        return (sum(list(map((lambda point: distance.euclidean(point,self.centroid)),self.old_points)))) / len(self.old_points) if len(self.old_points) else math.inf
 
     def __repr__(self):
         string = "Centroid: "
@@ -41,56 +44,63 @@ class Cluster:
             string += "".join(str(point)) + "\n"
         return string
 
+def efficiency(cls):
+    return sum(list(map((lambda cl: cl.calculate_avg_dist_to_centroid()),cls))) / len(cls)
 
 def generate_centroid():
     # pick random values within the limits of the dataset
-    return normalised_validation_data[random.randint(0, len(normalised_validation_data))]
+    return normalised_data[random.randint(0, len(normalised_data)-1)]
 
 
 def get_centroids(k):
     centroids = []
-    for i in range(0, k):
+    while len(centroids) != k:
         centroid = generate_centroid()
         if centroid not in centroids:  # so we don't pick the same centroid twice
             centroids.append(centroid)
-        else:
-            i -= 1
     return centroids
 
 
-def check_all_clusters_changed(clusters):
-    for cluster in clusters:
-        if cluster.changed:
-            return True
-    return False
+def any_cluster_changed(cls):
+    #check if any of the clusters has changed, if so return true
+    return True in list(map((lambda cl: cl.changed),cls))
 
 
 def kMeans(cls):
-    while check_all_clusters_changed(clusters):
-        for point in normalised_validation_data:  # go through all points
-            distances = []
-            for cl in cls:  # calculate distance to each centroid
-                # add distance from point to current centroid, and the id of the cluster of that centroid
-                distances.append([distance.euclidean(point, cl.centroid), cl.cluster_id])
-            distances.sort()  # sort distances so that the shortest will come first
+    while any_cluster_changed(cls):
+        for point in normalised_data:  # go through all points
+            # add distance from point to current centroid, and the id of the cluster of that centroid
+            distances = sorted(list(map((lambda cl: [distance.euclidean(point,cl.centroid),cl.cluster_id]),cls)))
             for cl in cls:  # check which cluster corresponds to the cluster_id with the shortest distance
                 if cl.cluster_id == distances[0][1]:
                     cl.points.append(point)  # add point to list of points of cluster
                     break
         # so now we have clustered every point around a centroid
         # let's recalculate each centroid based on its assigned points
-        for cluster in clusters:
-            print(cluster)
+        for cluster in cls:
             cluster.recalculate_centroid()
-            print(cluster)
             cluster.clear_points()
-            print(cluster)
+    return efficiency(cls)
+
+def kmeans_mult(cls,num):
+    return (sorted(list(map(kMeans,[cls]*num))))[0] if num > 0 else math.inf
+
+def optimal_k(recalculate_num=1):
+    k = 1
+    efficiency_results = [kmeans_mult(init(k),recalculate_num)]
+    while True:
+        k += 1
+        efficiency_results.append(kmeans_mult(init(k),recalculate_num))
+        temp = np.diff(efficiency_results, 2)
+        if temp.size > 0 and temp[-1] <= 0:
+            k -= 1
+            print(efficiency_results)
+            print(temp)
+            break
+    return k
+
+def init(k):
+    return list(map((lambda xs: Cluster(xs[1],xs[0])),list(enumerate(get_centroids(k)))))
 
 
-k = 4
-cts = get_centroids(k)
-clusters = []
-for i in range(0, k):
-    clusters.append(Cluster(cts[i], i))
-
-kMeans(clusters)
+print(optimal_k(30))
