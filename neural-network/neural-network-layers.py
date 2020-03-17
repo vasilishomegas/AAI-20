e8243a38c42c5cb94a9b5c1322326c8ad5d5e1fd
StienoX <stein.bout@xs4all.nl>
2020-03-17 14:49:41 +0100

    Added loops for the structure of calculation of the diffrend formulas

--- neural-network/neural-network-layers.py
+++ neural-network/neural-network-layers.py
@@ -26,7 +26,9 @@ def derived_tanh(x):
     return 1 - x*x
 
 class Neuron:
-
+    def __init__(self):
+        self.a = None
+        self.delta = None
 
 class NeuralNetwork:
     def __init__(self, network, function, derivative_function, learning_rate):
@@ -36,7 +38,6 @@ class NeuralNetwork:
         self.connections = {}
         self.biases = {}
 
-        #self.network = list(map(lambda x: ))
 
         for layer in network:
             self.network.append([Neuron() for _ in range(layer)])
@@ -49,12 +50,27 @@ class NeuralNetwork:
                     self.connections[(previous_neuron, neuron)] = random.uniform(-1.0,1)
 
     def run(self, inputs):
-        for input_neuron, value in zip(self.network[0],inputs):
+        for input_neuron, value in zip(self.network[0], inputs):
             input_neuron.a = value
-        for previous_layer, layer in zip(self.network[:-1],self.network[1:]):
+        for previous_layer, layer in zip(self.network[:-1], self.network[1:]):
             for neuron in layer:
                 for previous_neuron in previous_layer:
-                    # neuron.delta = self.connections[(previous_neuron, neuron)]
-                    # delta rule here!
+                    #  = self.connections[(previous_neuron, neuron)]
+                    # calculate a of all nodes here!
                     continue
+
+    def train(self, inputs_list, outputs_list):
+        for inputs, outputs in zip(inputs_list, outputs_list):
+            self.run(inputs)
+            for neuron, output in zip(self.network[-1], outputs):
+                # calculate the error of output neurons here
+                continue
+            for layer in reversed(self.network[:-1]):
+                for neuron in layer:
+                    # calculate weight and baises
+            for layer in self.network:
+                for neuron in layer:
+                    # update weight and baises
+
+
 
