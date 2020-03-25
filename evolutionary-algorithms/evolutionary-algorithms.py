b12f09fa50e7b8438f936527bd1b5959b37dc3a4
StienoX <stein.bout@xs4all.nl>
2020-03-25 23:26:31 +0100

    Added some crossover and mutation in the evolution function

--- evolutionary-algorithms/evolutionary-algorithms.py
+++ evolutionary-algorithms/evolutionary-algorithms.py
@@ -1,10 +1,11 @@
 from functools import reduce
 from operator import mul
+import copy
 import random
 
 
 def fitness_function(pile_0, pile_1):
-    return abs(((pile_0-36)*10+(pile_1-360))**2)
+    return -abs(((pile_0-36)*10+(pile_1-360)))
 
 
 def add36(lst):
@@ -55,18 +56,22 @@ class Evolutionary_Algortithm:
             self.current_evolution.append(genotype)
         for genotype in self.current_evolution:
             genotype.calculate_fitness()
-     #   for genotype in self.current_evolution:
-     #       print(genotype.fitness)
         self.current_evolution = sort_genotypes(self.current_evolution)
-       # for genotype in self.current_evolution:
-       #         print(genotype.fitness)
+        print(self.current_evolution[0].fitness)
 
     def evolve(self, amount=100):
         for _ in range(amount):
-            self.current_evolution = self.evolution_function(self.current_evolution, self.batch_size)
             for genotype in self.current_evolution:
                 genotype.calculate_fitness()
             self.current_evolution = sort_genotypes(self.current_evolution)
+            next_generation = copy.deepcopy(self.current_evolution[:int(len(self.current_evolution)*0.2)])
+            mutate_generation = copy.deepcopy(mutate(self.current_evolution[int(len(self.current_evolution)*0.2):int(len(self.current_evolution)*0.8)], 4))
+            crossover_generation = copy.deepcopy(mutate(crossover(self.current_evolution[:int(len(self.current_evolution)*0.2)], 1), 1))
+            self.current_evolution = next_generation + mutate_generation + crossover_generation
+
+        for genotype in self.current_evolution:
+                genotype.calculate_fitness()
+        self.current_evolution = sort_genotypes(self.current_evolution)
 
         return self.current_evolution
 
@@ -80,22 +85,19 @@ def mutate(batch, mutations):
             # not 100% tested
             genotype.set_bit(nr, (1 ^ genotype.get_bit(nr)))
             # genotype.bitstring |= (1 ^ ((genotype.bitstring >> nr) & 1)) << nr
+    return batch
 
 
-def crossover(batch, mutations=1, survivor_rate=0.2):
-    new_batch = []
-    target_len = len(batch)
-    current_batch = batch[:int(len(batch)*survivor_rate)]
-    batch_len = len(current_batch)
-    while len(new_batch) < target_len:
-
-        genotype = current_batch[random.randint(0, batch_len - 1)]
-        other_genotype = current_batch[random.randint(0, batch_len - 1)]
+def crossover(batch, mutations):
+    for _ in range(len(batch)):
+        genotype = batch[random.randint(0, len(batch) - 1)]
+        other_genotype = batch[random.randint(0, len(batch) - 1)]
         for _ in range(mutations):
             i = random.randint(0, 10)
             genotype.set_bit(i, other_genotype.get_bit(i))
-        new_batch.append(genotype)
-    return new_batch
+            other_genotype.set_bit(i, genotype.get_bit(i))
+
+    return batch
 
 
 
@@ -104,11 +106,14 @@ def sort_genotypes(genotypes: [Genotype]):
     result = [genotypes[0]]
     for genotype in genotypes[1:]:
         for i in range(len(result)):
-            if result[i].fitness > genotype.fitness:
+            if result[i].fitness < genotype.fitness:
                 result = result[:i] + [genotype] + result[i:]
                 break
             if i == len(result) - 1:
                 result.append(genotype)
+    for genotype in result:
+        print(genotype.fitness)
+    print()
     return result
 
 
@@ -120,10 +125,9 @@ def evolution_function_0(evolution, batch_size):
     return
 
 def main():
-    ea = Evolutionary_Algortithm(crossover, 300)
-    best = ea.evolve()
-    for genotype in best:
-        print(genotype.fitness)
+    ea = Evolutionary_Algortithm(mutate, 400)
+    print(ea.evolve(100)[0].fitness)
+
 
 if __name__ == '__main__':
     main()
