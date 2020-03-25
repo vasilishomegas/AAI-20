e8e1484576fd843f930ec0923a605a3d6b381f6b
StienoX <stein.bout@xs4all.nl>
2020-03-25 22:14:59 +0100

    WIP: crossover function, Added set and get bit

--- evolutionary-algorithms/evolutionary-algorithms.py
+++ evolutionary-algorithms/evolutionary-algorithms.py
@@ -4,7 +4,7 @@ import random
 
 
 def fitness_function(pile_0, pile_1):
-    return -(((pile_0-36)*10+(pile_1-360))**2)
+    return abs(((pile_0-36)*10+(pile_1-360))**2)
 
 
 def add36(lst):
@@ -32,11 +32,15 @@ class Genotype:
                 sum36 += (bitmask + 1)
         self.fitness = fitness_function(sum36, mul360)
 
-    def set_bit(self, pos, value):
-        pass
+    def set_bit(self, index, value):
+        mask = 1 << index
+        self.bitstring &= ~mask
+        if value:
+            self.bitstring |= mask
+        return self.bitstring
 
-    def get_bit(self, pos):
-        pass
+    def get_bit(self, index):
+        return self.bitstring >> index & 1
 
 class Evolutionary_Algortithm:
     def __init__(self, evolution_function, batch_size=10):
@@ -46,14 +50,23 @@ class Evolutionary_Algortithm:
         for _ in range(batch_size):
             genotype = Genotype()
             for i in range(10):
-                genotype.bitstring &= (random.randint(0, 1) << i)
+                genotype.bitstring |= (random.randint(0, 1) << i)
             self.current_evolution.append(genotype)
-        map(Genotype.calculate_fitness, self.current_evolution)
+        for genotype in self.current_evolution:
+            genotype.calculate_fitness()
+     #   for genotype in self.current_evolution:
+     #       print(genotype.fitness)
+        self.current_evolution = sort_genotypes(self.current_evolution)
+       # for genotype in self.current_evolution:
+       #         print(genotype.fitness)
 
     def evolve(self, amount=100):
         for _ in range(amount):
             self.current_evolution = self.evolution_function(self.current_evolution, self.batch_size)
-            map(Genotype.calculate_fitness, self.current_evolution)
+            for genotype in self.current_evolution:
+                genotype.calculate_fitness()
+            self.current_evolution = sort_genotypes(self.current_evolution)
+
         return self.current_evolution
 
 
@@ -67,27 +80,34 @@ def mutate(batch, mutations):
             genotype.bitstring |= (1 ^ ((genotype.bitstring >> nr) & 1)) << nr
 
 
-def crossover(batch, mutations, survivor_rate=0.2):
+def crossover(batch, mutations=1, survivor_rate=0.2):
     new_batch = []
+    target_len = len(batch)
     current_batch = batch[:int(len(batch)*survivor_rate)]
-    while len(new_batch) < len(batch):
+    batch_len = len(current_batch)
+    while len(new_batch) < target_len:
 
-        genotype = current_batch[random.randint(0, len(current_batch))]
-        other_genotype = current_batch[random.randint(0, len(current_batch))]
+        genotype = current_batch[random.randint(0, batch_len - 1)]
+        other_genotype = current_batch[random.randint(0, batch_len - 1)]
         for _ in range(mutations):
-            i = random.randint(1, 10)
-            genotype.bitstring |= (1 ^ ((other_genotype.bitstring >> i) & 1)) << i
+            i = random.randint(0, 10)
+            genotype.set_bit(i, other_genotype.get_bit(i))
+        new_batch.append(genotype)
+    return new_batch
+
+
 
 
 def sort_genotypes(genotypes: [Genotype]):
-    result = genotypes[0]
+    result = [genotypes[0]]
     for genotype in genotypes[1:]:
-        for i in range(result):
-            if result[i].fitness < genotype.fitness:
+        for i in range(len(result)):
+            if result[i].fitness > genotype.fitness:
                 result = result[:i] + [genotype] + result[i:]
                 break
             if i == len(result) - 1:
                 result.append(genotype)
+    return result
 
 
 def evolution_function_0(evolution, batch_size):
@@ -98,9 +118,10 @@ def evolution_function_0(evolution, batch_size):
     return
 
 def main():
-    ea = Evolutionary_Algortithm(evolution_function_0)
-    print(ea.evolve())
-
+    ea = Evolutionary_Algortithm(crossover, 300)
+    best = ea.evolve()
+    for genotype in best:
+        print(genotype.fitness)
 
 if __name__ == '__main__':
     main()
