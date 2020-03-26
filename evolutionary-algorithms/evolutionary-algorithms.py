0ec4470fc34331761e0de701934117e6d602e692
Willem <willembike@gmail.com>
2020-03-26 22:42:06 +0100

    Added comments to code

--- evolutionary-algorithms/evolutionary-algorithms.py
+++ evolutionary-algorithms/evolutionary-algorithms.py
@@ -8,14 +8,6 @@ def fitness_function(pile_0, pile_1):
     return -abs(((pile_0-36)*10+(pile_1-360)))
 
 
-def add36(lst):
-    return sum(lst)
-
-
-def mult360(lst):
-    return reduce(mul, lst)
-
-
 class Genotype:
     def __init__(self):
         self.bitstring = 0
@@ -43,8 +35,7 @@ class Genotype:
 
 
 class Evolutionary_Algortithm:
-    def __init__(self, evolution_function, batch_size=10):
-        self.evolution_function = evolution_function
+    def __init__(self, batch_size=10):
         self.batch_size = batch_size
         self.current_evolution = []
         for _ in range(batch_size):
@@ -60,9 +51,13 @@ class Evolutionary_Algortithm:
         for _ in range(amount):
             for genotype in self.current_evolution:
                 genotype.calculate_fitness()
+            # Make sure those of the population with the best fitness are ranked first
             self.current_evolution = sort_genotypes(self.current_evolution)
+            # The best 20%, in our case, will automatically qualify for the next generation
             next_generation = copy.deepcopy(self.current_evolution[:int(len(self.current_evolution)*0.2)])
+            # We mutate the next 60%, hoping it will lead to random improvement
             mutate_generation = copy.deepcopy(mutate(self.current_evolution[int(len(self.current_evolution)*0.2):int(len(self.current_evolution)*0.8)], 4))
+            # We apply crossover and mutation to the bottom 20%, with crossover being done with the best 20%
             crossover_generation = copy.deepcopy(mutate(crossover(self.current_evolution[:int(len(self.current_evolution)*0.2)], 1), 1))
             self.current_evolution = next_generation + mutate_generation + crossover_generation
 
@@ -79,22 +74,23 @@ def mutate(batch, mutations):
         for _ in range(mutations):
             nr = random.randint(0, 10)
             # isolate a (random) bit, flip it and put it back in place
-            # not 100% tested
             genotype.set_bit(nr, (1 ^ genotype.get_bit(nr)))
-            # genotype.bitstring |= (1 ^ ((genotype.bitstring >> nr) & 1)) << nr
     return batch
 
 
 def crossover(batch, mutations):
     # oneway crossover function
     for _ in range(len(batch)):
+        # choose two genotype for crossover
         genotype = batch[random.randint(0, len(batch) - 1)]
         other_genotype = batch[random.randint(0, len(batch) - 1)]
         for _ in range(mutations):
+            # pick a random bit
             i = random.randint(0, 10)
+            # take the value of that bit from one genotype and write it to the other
             genotype.set_bit(i, other_genotype.get_bit(i))
-
     return batch
+
 
 def sort_genotypes(genotypes: [Genotype]):
     result = [genotypes[0]]
@@ -112,9 +108,8 @@ def sort_genotypes(genotypes: [Genotype]):
 
 
 def main():
-
-    ea = Evolutionary_Algortithm(mutate, 10)
-    print(ea.evolve(100)[0].fitness)
+    ea = Evolutionary_Algortithm()
+    print(ea.evolve()[0].fitness)
 
 
 if __name__ == '__main__':
